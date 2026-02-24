0e5db3727c2bfa3f80d6f25eafd7705d41eadb3b
Paweł Skierś <pawelsk269@gmail.com>
2026-02-24 19:59:56 +0100

    Add pt return type

--- src/diffusers/pipelines/ddpm/pipeline_ddpm.py
+++ src/diffusers/pipelines/ddpm/pipeline_ddpm.py
@@ -131,6 +131,8 @@ class DDPMPipeline(DiffusionPipeline):
             if XLA_AVAILABLE:
                 xm.mark_step()
 
+        if output_type == "pt":
+            return ImagePipelineOutput(images=image)
         image = (image / 2 + 0.5).clamp(0, 1)
         image = image.cpu().permute(0, 2, 3, 1).numpy()
         if output_type == "pil":
