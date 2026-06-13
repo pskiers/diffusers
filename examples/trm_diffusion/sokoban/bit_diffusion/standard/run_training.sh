f52bb9249785ecbe68533eddfcce0ec02921d493
mgrzanka <01186969@pw.edu.pl>
2026-06-13 11:56:06 +0200

    change slurm script to athena and add requirements

--- examples/trm_diffusion/sokoban/bit_diffusion/standard/run_training.sh
+++ examples/trm_diffusion/sokoban/bit_diffusion/standard/run_training.sh
@@ -5,20 +5,19 @@
 #SBATCH --cpus-per-task=72
 #SBATCH --mem=120G
 #SBATCH --time=24:00:00
-#SBATCH --account=plgdynamic3-gpu-gh200
-#SBATCH --partition=plgrid-gpu-gh200
+#SBATCH --account=plgdyplomancipw3tt-gpu-a100
+#SBATCH --partition=plgrid-gpu-a100
 #SBATCH --gres=gpu:1
-#SBATCH --output=/net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/slurm_outputs/stdout/sokoban_std_%j.out
-#SBATCH --error=/net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/slurm_outputs/stderr/sokoban_std_%j.err
+#SBATCH --output=/net/tscratch/people/plgmgrzanka/trm_sokoban/stdout/sokoban_std_%j.out
+#SBATCH --error=/net/tscratch/people/plgmgrzanka/trm_sokoban/stderr/sokoban_std_%j.err
 
-module load ML-bundle/24.06a
-source /net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/venv/bin/activate
+module load Miniconda3/23.3.1-0
+source /net/tscratch/people/plgmgrzanka/trm_sokoban/venv/bin/activate
 
-export SSL_CERT_FILE=/net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/cacert.pem
 export PYTHONUNBUFFERED=1
 
-export PYTHONPATH=$PYTHONPATH:/net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/diffusers/src:/net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/diffusers/examples/trm_diffusion
+export PYTHONPATH=$PYTHONPATH:/net/tscratch/people/plgmgrzanka/trm_sokoban/diffusers/src:/net/tscratch/people/plgmgrzanka/trm_sokoban/diffusers/examples/trm_diffusion
 
-cd /net/scratch/hscra/plgrid/plgmgrzanka/trm_sokoban/diffusers/examples/trm_diffusion
+cd /net/tscratch/people/plgmgrzanka/trm_sokoban/diffusers/examples/trm_diffusion
 
 srun python sokoban/bit_diffusion/standard/train_std.py
